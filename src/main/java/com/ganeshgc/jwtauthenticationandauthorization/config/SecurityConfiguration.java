c8543ce07b4b4f7557b9472c49c12a940bd377f5
ganeshema <ganeshlema2021@gmail.com>
2024-08-12 15:52:05 -0500

    implementation of authentication and authorization using jwt token

---
+++ src/main/java/com/ganeshgc/jwtauthenticationandauthorization/config/SecurityConfiguration.java
@@ -0,0 +1,49 @@
+package com.ganeshgc.jwtauthenticationandauthorization.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+import static com.ganeshgc.jwtauthenticationandauthorization.user.Permission.*;
+import static com.ganeshgc.jwtauthenticationandauthorization.user.Role.ADMIN;
+import static com.ganeshgc.jwtauthenticationandauthorization.user.Role.MEMBER;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity
+public class SecurityConfiguration {
+
+    private final AuthenticationProvider authenticationProvider;
+    private final JwtAuthFilter jwtAuthFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req ->
+                        req.requestMatchers("/v1/auth/*")
+                                .permitAll()
+                                .requestMatchers("/v1/management/**").hasAnyRole(ADMIN.name(), MEMBER.name())
+                                .requestMatchers(GET, "/v1/management/**").hasAnyAuthority(ADMIN_READ.name(), MEMBER_READ.name())
+                                .requestMatchers(POST, "/v1/management/**").hasAnyAuthority(ADMIN_CREATE.name(), MEMBER_CREATE.name())
+                                .anyRequest()
+                                .authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+}
---
+++ src/test/java/com/ganeshgc/jwtauthenticationandauthorization/JwtAuthenticationAndAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ganeshgc.jwtauthenticationandauthorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthenticationAndAuthorizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
